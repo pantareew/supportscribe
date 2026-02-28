267b83ba6d96f1e0051ee2f57c555893eeedf775
pantareew <pantaree1459@gmail.com>
2026-02-28 20:28:36 +1100

    handling mic access denied

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -24,19 +24,25 @@ export default function Home() {
     //check for ws
     if (!socket) return;
     //browser asks permission for microphone
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true }); //get only microphone access
-    const mediaRecorder = new MediaRecorder(stream); //set up recorder by connecting to stream
-    mediaRecordRef.current = mediaRecorder; //store data in ref so that stoprecording can access it too
-    //event handler to run every time the recorder has a new chunk
-    mediaRecorder.ondataavailable = (event) => {
-      //check for data blob and ws state
-      if (event.data.size > 0 && socket.readyState === WebSocket.OPEN) {
-        socket.send(event.data); //send audio blob to backend immediately
-      }
-    };
-    mediaRecorder.start(2000); //start recording and break it into chunks of 2 secs
-    setRecording(true); //ui shows end call btn
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true }); //connect to mic access
+      const mediaRecorder = new MediaRecorder(stream); //set up recorder by connecting to stream
+      mediaRecordRef.current = mediaRecorder; //store data in ref so that stoprecording can access it too
+      //event handler to run every time the recorder has a new chunk
+      mediaRecorder.ondataavailable = (event) => {
+        //check for data blob and ws state
+        if (event.data.size > 0 && socket.readyState === WebSocket.OPEN) {
+          socket.send(event.data); //send audio blob to backend immediately
+        }
+      };
+      mediaRecorder.start(2000); //start recording and break it into chunks of 2 secs
+      setRecording(true); //ui shows end call btn
+    } catch (err) {
+      console.error("Microphone access denied", err);
+      alert("Please allow microphone access to use SupportScribe");
+    }
   };
+
   const stopRecording = () => {
     mediaRecordRef.current?.stop(); //stop recording
     setRecording(false); //ui switch to start call btn
