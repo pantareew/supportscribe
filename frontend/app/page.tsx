2abfc35abc1733515b04aa36227d408ef217d30f
pantareew <pantaree1459@gmail.com>
2026-02-28 20:22:00 +1100

    set up stopRecording

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -27,15 +27,20 @@ export default function Home() {
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true }); //get only microphone access
     const mediaRecorder = new MediaRecorder(stream); //set up recorder by connecting to stream
     mediaRecordRef.current = mediaRecorder; //store data in ref so that stoprecording can access it too
+    //event handler to run every time the recorder has a new chunk
     mediaRecorder.ondataavailable = (event) => {
+      //check for data blob and ws state
       if (event.data.size > 0 && socket.readyState === WebSocket.OPEN) {
-        socket.send(event.data);
+        socket.send(event.data); //send audio blob to backend immediately
       }
     };
     mediaRecorder.start(2000); //start recording and break it into chunks of 2 secs
-    setRecording(true);
+    setRecording(true); //ui shows end call btn
   };
-
+  const stopRecording = () => {
+    mediaRecordRef.current?.stop(); //stop recording
+    setRecording(false); //ui switch to start call btn
+  };
   return (
     <div style={{ padding: 20 }}>
       <h1>SupportScribe WebSocket Test</h1>
