1b77af0a2fc15877026dbd3f1e9a461a461a40c2
pantareew <pantaree1459@gmail.com>
2026-02-28 19:47:23 +1100

    startrecording setup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,55 +1,50 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const [socket, setSocket] = useState<WebSocket | null>(null); //websocket connection instance
-  const [messages, setMessages] = useState<string[]>([]); //all messages from backend
-  const [input, setInput] = useState(""); //input from frontend
+  const [recording, setRecording] = useState(false); //show start or end button
+  const mediaRecordRef = useRef<MediaRecorder | null>(null); //stores media recorder
 
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:8000/ws"); //connect to fastapi websocket endpoint
-
+    //connect successful
     ws.onopen = () => {
-      //run when connects
       console.log("Connected to backend");
     };
-
-    ws.onmessage = (event) => {
-      //run when backend send sth
-      setMessages((prev) => [...prev, event.data]);
-    };
-
-    setSocket(ws); //for sending msg to backend
+    setSocket(ws); //set ws for sending data to backend
 
     return () => {
       ws.close(); //close connection when page refreshes
     };
   }, []);
 
-  const sendMessage = () => {
-    if (socket && input) {
-      socket.send(input); //send msg to backend thru ws
-      setInput(""); //clear data
-    }
+  const startRecording = async () => {
+    //check for ws
+    if (!socket) return;
+    //browser asks permission for microphone
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true }); //get only microphone access
+    const mediaRecorder = new MediaRecorder(stream); //set up recorder by connecting to stream
+    mediaRecordRef.current = mediaRecorder; //store data in ref so that stoprecording can access it too
+    mediaRecorder.ondataavailable = (event) => {
+      if (event.data.size > 0 && socket.readyState === WebSocket.OPEN) {
+        socket.send(event.data);
+      }
+    };
+    mediaRecorder.start(2000); //start recording and break it into chunks of 2 secs
+    setRecording(true);
   };
 
   return (
     <div style={{ padding: 20 }}>
       <h1>SupportScribe WebSocket Test</h1>
-      {/*message sending to backend */}
-      <input
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="Type message"
-      />
-      <button onClick={sendMessage}>Send</button>
-      {/*messages received from backend */}
-      <div>
-        {messages.map((msg, index) => (
-          <p key={index}>{msg}</p>
-        ))}
-      </div>
+      {/*recording */}
+      {!recording ? (
+        <button onClick={startRecording}>Start Call</button>
+      ) : (
+        <button onClick={stopRecording}>End Call</button>
+      )}
     </div>
   );
 }
