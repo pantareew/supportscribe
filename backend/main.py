bb36780384d526f0b8bd101cae26a87b95a8703c
pantareew <pantaree1459@gmail.com>
2026-04-20 22:24:38 +1000

    generate summary function

--- backend/main.py
+++ backend/main.py
@@ -36,12 +36,34 @@ async def transcribe_audio(audio_bytes: bytes) -> str: #returning a string (tran
         file=audio_file 
     )
     return transcript.text
+
+#send transcript to AI to generate summary
+async def generate_summary(transcript: str) -> str: #return summary text
+    #response return in structured format 
+    response = await asyncio.to_thread(     #run function in separate thread so server wont freeze while waiting for ai
+        ai.responses.create,
+        model="gpt-4.1-mini",
+        #prompt
+        input=f"""
+        You are an AI assistant for IT service desk agents.
+        Given this support call transcript:
+        {transcript}
+        Generate:
+        1. Issue Summary
+        2. Troubleshooting Steps Taken
+        3. Resolution Status
+        4. Next Steps (if unresolved)
+        Keep it concise and structured.
+        """
+    ) 
+    return response.output[0].content[0].text #first result from output[], first content[], actual text
 #websocket connection
 @app.websocket("/ws") #websocket endpoint
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept() #server accept connection
     audio_chunk = bytearray() #array to hold audio data
     webm_header = None #to store frist chunk header
+    full_transcript = "" #full transcript for summary
     try:
         while True: #while connected
             data = await websocket.receive_bytes() #await for frontend to send data
@@ -50,10 +72,11 @@ async def websocket_endpoint(websocket: WebSocket):
                 webm_header = data[:2000] #first 2 kb contains headers 
             audio_chunk.extend(data) #add received data to array
             print("Chunk size:", len(audio_chunk))
-            if len(audio_chunk) > 200_000: #receive enough data to transcribe (200_000 bytes)
+            if len(audio_chunk) > 60_000: #receive enough data to transcribe
                 try:
                     chunk_to_transcribe = webm_header + audio_chunk #add header to audio chunk to prevent type error
                     text = await transcribe_audio(chunk_to_transcribe) #call whisper to get convert bytes to text
+                    full_transcript += " " + text #accumulate full transcript
                     await websocket.send_text(text) #send transcribed text back to frontend
                 except Exception as e:
                     await websocket.send_text(f"[Error transcribing: {e}]")
